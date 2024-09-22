57544b7e73755fbdbfc417350b030f6730080135
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2024-09-22 22:24:33 +0530

    leetcode

---
+++ fib.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+#include <vector>
+using namespace std;
+int fibonacci(int n,vector<int>& mem)
+{
+    if(n<=1)
+        return mem[n]=n;
+    if(mem[n] != 0)
+        return mem[n];
+    mem[n] = fibonacci(n-1,mem) + fibonacci(n-2,mem);
+    return mem[n];
+}
+
+
+
+int main()
+{
+    
+    cout<<"enter a number";
+    int n;
+    cin>>n;
+    vector<int> mem(n+1,0); 
+    fibonacci(n,mem);
+    for(int i = 0; i <= n; i++)
+        cout << mem[i] << " ";
+    return 0;
+}
